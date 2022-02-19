d68bf5b7eaf34b59761683c4fe9d997473d5db67
Tengiz Sharafiev <btolfa@gmail.com>
2022-02-19 20:57:15 +0700

    Add false and tests

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -5,3 +5,9 @@ fn true_ok() {
     let mut cmd = Command::cargo_bin("true").unwrap();
     cmd.assert().success();
 }
+
+#[test]
+fn false_non_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
